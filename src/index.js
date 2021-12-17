6bdc17dc2a2dc34761d5b6d1c13c914cf1563616
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2021-12-16 21:44:03 -0300

    1.12: anecdotes step1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+const Anecdote = ({ content }) => 
+  <p>{content}</p>
+
+const Button = ({ handler }) =>
+  <button onClick={handler}>Next anecdote</button>
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0);
+  const nextAnecdote = generateNext(selected);
+
   return (
-    <h1>Hello, world!</h1>
-  );
+    <div>
+      <Anecdote content={anecdotes[selected]} />
+      <Button handler={() => setSelected(nextAnecdote)} />
+    </div>
+  )
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+function generateNext(current) {
+  let value = generateRandomInt(0, 6);
+  while (value === current) {
+    value = generateRandomInt(0, 6);
+  }
+  return value;
+}
+
+function generateRandomInt(max, min) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+ReactDOM.render(<App anecdotes={anecdotes} />, 
   document.getElementById('root')
 );
